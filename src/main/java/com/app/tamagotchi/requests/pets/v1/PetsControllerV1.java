8ca62d35435df28e37e086f7dd7136b98fe4df05
Pieter <23745584@g.nwu.ac.za>
2021-06-14 22:03:58 +0200

    test 3

--- src/main/java/com/app/tamagotchi/requests/pets/v1/PetsControllerV1.java
+++ src/main/java/com/app/tamagotchi/requests/pets/v1/PetsControllerV1.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.lang.Exception;
-import io.sentry.Sentry;
+
 
 
 @RestController
