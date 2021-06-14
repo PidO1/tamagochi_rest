68a13cf01c1e9a70874c3718a7248064f6260cb9
Pieter <23745584@g.nwu.ac.za>
2021-06-14 21:18:55 +0200

    sentry main

--- src/main/java/com/app/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/app/tamagotchi/TamagotchiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import java.lang.Exception;
+import io.sentry.Sentry;
 
 @SpringBootApplication
 @EnableTransactionManagement
