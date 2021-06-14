57cc2631bd16ddd474fa725e149edaa3d125b381
Pieter <23745584@g.nwu.ac.za>
2021-06-14 22:22:36 +0200

    test 5

--- src/main/java/com/app/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/app/tamagotchi/TamagotchiApplication.java
@@ -7,7 +7,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import java.lang.Exception;
 import io.sentry.Sentry;
+import io.sentry.spring.EnableSentry;
+// NOTE: Replace the test DSN below with YOUR OWN DSN to see the events from this app in your Sentry
+// project/dashboard
 
+
+@EnableSentry(dsn = "https://dddd694ff5c94e13b686e50ba44f5bc0@o842130.ingest.sentry.io/5813958")// https://dddd694ff5c94e13b686e50ba44f5bc0@o842130.ingest.sentry.io/5813958
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
