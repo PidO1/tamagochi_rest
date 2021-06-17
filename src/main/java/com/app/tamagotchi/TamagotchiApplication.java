5d3ebb7183f14730641c1d9bd4efebc8999f6df7
Pieter <42219172+PidO1@users.noreply.github.com>
2021-06-17 09:56:27 +0200

    Update TamagotchiApplication.java

--- src/main/java/com/app/tamagotchi/TamagotchiApplication.java
+++ src/main/java/com/app/tamagotchi/TamagotchiApplication.java
@@ -8,8 +8,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import java.lang.Exception;
 import io.sentry.Sentry;
 import io.sentry.spring.EnableSentry;
-// NOTE: Replace the test DSN below with YOUR OWN DSN to see the events from this app in your Sentry
-// project/dashboard
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSentry(dsn = "https://dddd694ff5c94e13b686e50ba44f5bc0@o842130.ingest.sentry.io/5813958") // https://dddd694ff5c94e13b686e50ba44f5bc0@o842130.ingest.sentry.io/5813958
